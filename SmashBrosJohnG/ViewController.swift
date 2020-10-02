ddbc304b42053fdeedf71433ff84176909bdd39e
Ninja-Lombax <godzillakotm80@gmail.com>
2020-10-02 15:38:20 -0500

    Initial Commit

---
+++ SmashBrosJohnG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmashBrosJohnG
+//
+//  Created by John Grasser on 10/2/20.
+//  Copyright © 2020 John Grasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
